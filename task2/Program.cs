2714d3c3a2b3400567694e4a151193784473ddc5
Nikolay <fufaiker@gmail.com>
2022-06-07 22:19:16 +0300

    second task

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numFirst  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numThree = Convert.ToInt32(Console.ReadLine());
+
+int max = numFirst;
+if (numSecond > max) max = numSecond;
+if (numThree  > max) max = numThree;
+
+Console.WriteLine("Максимальное число: " + max);
