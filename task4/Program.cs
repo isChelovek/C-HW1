3fc0e95f3acf5d2e76c8adf7d577868c5487ee7b
Nikolay <fufaiker@gmail.com>
2022-06-07 22:20:24 +0300

    tourth task

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= num)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i + " ");
+    }
+    i++;
+}
