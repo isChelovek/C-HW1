de8ac870f992efb75f04477f22001393b633907f
Nikolay <fufaiker@gmail.com>
2022-06-07 22:18:58 +0300

    first task

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numFirst  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+if (numFirst > numSecond)
+{
+    Console.WriteLine("Первое число " + numFirst + " больше чем второе число " + numSecond);
+}
+else if (numFirst < numSecond)
+{
+    Console.WriteLine("Второе число " + numSecond + " больше чем первое число " + numFirst);
+}
+else 
+{
+    Console.WriteLine("Числа " + numFirst + " и " + numSecond + " равны");
+}
