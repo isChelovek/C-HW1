5b9e55d4843a80ecafb3d2720c4cc621e7ba17d0
Nikolay <fufaiker@gmail.com>
2022-06-07 22:19:29 +0300

    three task

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 != 0)
+{
+    Console.WriteLine("Число не четное");
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
